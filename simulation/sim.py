15d3d273169fa0fa3b7b862b84e1f2f410a15b7a
Victorine Buiten <buiten@strw.leidenuniv.nl>
2022-02-13 16:11:33 +0100

    Wrote distance calculator and started on simulator class

--- simulation/sim.py
+++ simulation/sim.py
@@ -2,3 +2,15 @@
 
 import numpy as np
 
+class NBodyWorker:
+    def __init__(self, bodies, box, timestep=0.1):
+
+        # check if box and bodies have the same dimensions
+        if box.dim != bodies.dim:
+            raise ValueError ("Dimensions of bodies and box do not match!")
+
+        self.bodies = bodies
+        self.box = box
+        self.time = 0
+        self.timestep = timestep
+
--- simulation/utils.py
+++ simulation/utils.py
@@ -2,3 +2,14 @@
 
 import numpy as np
 
+def distanceFromPosition(pos1, pos2):
+    '''Calculates the distance between 1D arrays pos1 and pos2.'''
+
+    diffVector = pos2 - pos1
+    dist = np.sqrt(np.dot(diffVector, diffVector))
+
+    return dist
+
+
+# need a function for evaluating the gradient of the Lennard-Jones potential
+# also need some class for storing constants and physical units
