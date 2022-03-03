f64c7648884f4b532d0c6bbd8cb5bd5f0163980b
Victorine Buiten <buiten@strw.leidenuniv.nl>
2022-03-03 17:31:04 +0100

    Implemented minimum image convention using the expression from lec 3

--- simulation/sim.py
+++ simulation/sim.py
@@ -34,7 +34,8 @@ class NBodyWorker:
                 # rather than a circle/sphere
                 pos_diff = pos_others - pos
                 length = self.box.lengths[0]
-                pos_others = pos_others - length * (pos_diff/length).astype(int)
+                pos_others = (pos - pos_others + length/2) % length - length/2
+                #pos_others = pos_others - length * (pos_diff/length).astype(int)
                 forces[i] = LennardJonesForce(pos, pos_others)
 
             # now update the positions
