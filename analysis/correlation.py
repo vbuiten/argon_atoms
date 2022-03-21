aefc80a6708804388df4e9215a5f40e66f414b07
vbuiten <buiten@strw.leidenuniv.nl>
2022-03-21 17:43:52 +0100

    Wrote code for computing and plotting a histogram of distances and the correlation function, both including ~1 sigma error bars

--- analysis/correlation.py
+++ analysis/correlation.py
@@ -10,40 +10,108 @@ from analysis.utils import PlotPreferences
 class DistanceHistogram:
     '''Create a histogram of the distances between pairs of particles.'''
 
-    def __init__(self, particles, box_lengths, bins, plotprefs=None):
+    def __init__(self, particles, box_lengths, bins=10, plotprefs=None):
 
         if isinstance(particles, framework.particles.Particles):
             particles = [particles]
 
-        n_atoms = particles[0].n_atoms
+        self.box_lengths = box_lengths
+        self.n_iterations = len(particles)
+        self.n_atoms = particles[0].n_atoms
+        self.dim = particles[0].dim
 
-        distances = np.array((len(particles), n_atoms, n_atoms-1))
+        if isinstance(self.box_lengths, np.ndarray) and len(self.box_lengths.shape) == 1:
+            self.volume = self.box_lengths[0]**self.dim
+
+        elif isinstance(self.box_lengths, float):
+            self.volume = self.box_lengths**self.dim
+
+        else:
+            raise ValueError("Invalid box_lengths given. Give either a 1D array or a float.")
+
+        self.temperature = particles[0].temperature
+        self.density = self.n_atoms / self.volume
+
+        self.distances = np.zeros((len(particles), self.n_atoms, self.n_atoms-1))
 
         for idx, el in enumerate(particles):
 
-            distances[idx] = el.pairDistances(box_lengths)
+            self.distances[idx] = el.pairDistances(box_lengths)
 
-        self.counts, self.bin_edges = np.histogram(distances, bins)
+        if isinstance(bins, int):
+            min_edge, max_edge = self.distances.min(), self.distances.max()
+            width = (max_edge - min_edge)/bins
+            bin_edges = np.arange(min_edge, max_edge, width)
+
+        elif isinstance(bins, np.ndarray):
+            bin_edges = bins
+
+        else:
+            raise TypeError("Invalid bins. Give either the number of bins or the edges.")
+
+        self.counts, self.bin_edges = np.histogram(self.distances, bin_edges)
         self.bin_widths = self.bin_edges[1:] - self.bin_edges[:-1]
         self.bin_mids = self.bin_edges[:-1] + 0.5*self.bin_widths
+
+        counts_iterations = np.zeros((self.n_iterations, len(self.bin_mids)))
+
+        for i in range(self.n_iterations):
+            counts_iterations[i], _ = np.histogram(self.distances[i], bin_edges)
+
+        self.counts_avg = np.mean(counts_iterations, axis=0)
+        self.counts_68p = np.percentile(counts_iterations, [16.,84], axis=0)
+        self.counts_errs = np.abs(self.counts_avg - self.counts_68p)
+        self.counts_std = np.std(counts_iterations, axis=0)
 
         if plotprefs is None:
             self.plotprefs = PlotPreferences(markersize=5, marker="s")
 
+        self.fig, self.ax = plt.subplots(figsize=self.plotprefs.figsize, dpi=self.plotprefs.dpi)
+
+        self.ax.set_xlabel(r"Distance")
+        self.ax.set_ylabel(r"# of occurrences")
+
+
+    def plotTotal(self):
+
+        self.ax.plot(self.bin_mids, self.counts, marker=self.plotprefs.marker)
+
+
+    def plotIterationAveraged(self):
+
+        self.ax.plot(self.bin_mids, self.counts_avg, markersize=self.plotprefs.markersize, marker=self.plotprefs.marker,
+                     label="Mean")
+        self.ax.fill_between(self.bin_mids, self.counts_68p[0], self.counts_68p[1], alpha=0.3,
+                             label="68% confidence interval")
+
+        self.ax.set_title("Averaged over "+str(self.n_iterations)+" Realisations")
+        self.ax.legend()
+
+
+    def show(self):
+
+        self.fig.show()
+
+
+class CorrelationFunction(DistanceHistogram):
+    def __init__(self, particles, box_lengths, bins=10, plotprefs=None):
+        super().__init__(particles, box_lengths, bins, plotprefs)
+
+        factor1 = 2 * self.volume / (self.n_atoms * (self.n_atoms - 1))
+        factor2 = 1 / (4 * np.pi * self.bin_mids**2 * self.bin_widths)
+        self.g = factor1 * factor2 * self.counts_avg
+
+        self.g_error = factor1 * factor2 * self.counts_std
+
+        self.ax.set_ylabel(r"Correlation Function $g(r)$")
+        self.fig.suptitle("Correlation Function")
+        self.ax.set_title(r"$\rho = $"+str(np.around(self.density,3))+r"; $T = $"+str(np.around(self.temperature,3)))
 
     def plot(self):
 
-        fig, ax = plt.subplots(figsize=self.plotprefs.figsize, dpi=self.plotprefs.dpi)
-
-        ax.plot(self.bin_mids, self.counts, marker=self.plotprefs.marker)
-        ax.set_xlabel(r"Distance")
-        ax.set_ylabel(r"# of occurrences")
-
-        fig.suptitle(r"Pair Correlation Function $g(r)$")
-
-        self.fig = fig
-        self.ax = ax
-
+        self.ax.plot(self.bin_mids, self.g, marker=self.plotprefs.marker, markersize=self.plotprefs.markersize)
+        self.ax.fill_between(self.bin_mids, self.g+self.g_error, self.g-self.g_error, alpha=0.3,
+                             label=r"Estimated $1 \sigma$ error")
 
     def show(self):
 
---
+++ tests/several_realisations.py
@@ -0,0 +1,39 @@
+'''Test file for experimenting with running several realisations of the same initial conditions.'''
+
+from framework.box import BoxBase
+from framework.particles import Particles
+from simulation.sim import Simulator
+from analysis.correlation import DistanceHistogram, CorrelationFunction
+
+#savepath = "C:\\Users\\victo\\Documents\\Uni\\COP\\"
+savepath = "/net/vdesk/data2/buiten/COP/"
+
+n_atoms = 108
+iterations = 10
+temperature = 3.0
+density = 0.3
+
+box = BoxBase(density, n_atoms, 3)
+
+particle_sets = []
+
+for i in range(iterations):
+    atoms = Particles(n_atoms,3)
+    atoms.positions = box.edges
+    atoms.temperature = temperature
+
+    sim = Simulator(atoms, box, timestep=0.001)
+    sim.equilibrate(iteration_time=5, threshold=0.01)
+    sim.evolve(10, timestep_external=1., savefile=savepath+"liquid-iteration"+str(i+1)+".hdf")
+
+    particle_sets.append(atoms)
+
+    print ("Iteration "+str(i+1)+" finished.")
+
+distance_hist = DistanceHistogram(particle_sets, box.lengths, bins=100)
+distance_hist.plotIterationAveraged()
+distance_hist.show()
+
+corr_func = CorrelationFunction(particle_sets, box.lengths, bins=100)
+corr_func.plot()
+corr_func.show()
